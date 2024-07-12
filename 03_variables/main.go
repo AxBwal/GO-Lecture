8c9e3716093bd23fc4fa43ab423927b6255703ea
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-07-12 21:43:49 +0530

    3 rd lec

---
+++ 03_variables/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var name string="anshuman"
+
+	//In golang if you are declaring somethign and not using it then it will give you error
+	//lets say name is decalared but not printed ..then it will throw error
+
+	//golang will automatically infer the values type
+
+	//var name="akhil"
+
+	//short hand notaion
+	// name:="akhil"
+
+	var name string
+	name = "anshu"
+	fmt.Println(name)
+}
