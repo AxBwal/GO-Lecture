90a0e3d1074243cf1d0cf20354e94afda83bdd1d
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-07-16 07:33:29 +0530

    if else

---
+++ 06_if_else/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	age := 18
+
+	if age >= 18 {
+		fmt.Println("yes eligible to vote")
+	}else{
+		fmt.Println("not eligible to vote")
+	}
+
+	if number:=20; number >=20 {
+		fmt.Println("yes number is greater than 20")
+	}else if number==18 {
+		fmt.Println(" number is equal to 18")
+	}
+
+}
